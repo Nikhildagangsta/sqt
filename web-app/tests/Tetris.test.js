b2aa5886e0a0202103b11641f7f86471745931d7
Nikhildagangsta <92844742+Nikhildagangsta@users.noreply.github.com>
2022-08-17 23:04:12 +0100

    code for tetristest.js v1

--- web-app/tests/Tetris.test.js
+++ web-app/tests/Tetris.test.js
@@ -34,14 +34,44 @@ describe("Hold", function () {
         Then the game state before and after the second hold, is the same.`,
         function () {
             const initial_game = Tetris.hold(Tetris.new_game());
+            const held_game = Tetris.hold(initial_game);
+
+            if (!R.equals(initial_game, held_game)) {
+                throw new Error(
+                    `The inital and final game state do not match
+                    Initial: display game: ${JSON.stringify(initial_game)}
+                    Final: display game   ${JSON.stringify(held_game)}`
+                );
+            }
+
             // Implement the rest of this function.
         }
     );
 
     it(
-        `### Change this to your test description ###`,
+        `If there is no held piece and a hold performed, 
+        the **next tetromino** is deployed.
+        Given a Tetris game where held piece field is empty 
+        When we hold a piece;
+        Then the current tetrimino is equal to the previous next piece
+        `,
         function () {
-            // Implement this function.
+            const initial_game = Tetris.new_game();
+            const initial_piece = initial_game.current_tetromino;
+            const held_game = Tetris.hold(initial_game);
+
+            const held_current_tet = held_game.current_tetromino
+            const initial_next_tet = initial_game.next_tetromino
+
+            // You'll need to implement Tetris.hold before this works.
+            //const final_piece = final_game.current_tetromino;
+            if (!R.equals(held_game.current_tetromino, initial_game.next_tetromino)) {
+                throw new Error(
+                    `The inital and final tetrominos do not match
+                    Initial: ${JSON.stringify(held_current_tet)}
+                    Final:   ${JSON.stringify(initial_next_tet)}`
+                );
+            }
         }
     );
 });
