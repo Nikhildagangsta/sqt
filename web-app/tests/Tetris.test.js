c063544ffcb0cd1123340d88893f0bd3f81e1a77
Nikhildagangsta <92844742+Nikhildagangsta@users.noreply.github.com>
2022-08-19 13:38:51 +0100

    tetris.testjs v2

--- web-app/tests/Tetris.test.js
+++ web-app/tests/Tetris.test.js
@@ -8,12 +8,12 @@ describe("Hold", function () {
         When the sequence Hold > Hard Drop > Hold is performed;
         Then the current tetromino before and after the sequence, is the same.`,
         function () {
-            const initial_game = Tetris.new_game();
-            const initial_piece = initial_game.current_tetromino;
+            const initial_game_state = Tetris.new_game();
+            const initial_piece = initial_game_state.current_tetromino;
             // You'll need to implement Tetris.hold before this works.
             const final_game = Tetris.hold(
                 Tetris.hard_drop(
-                    Tetris.hold(initial_game)
+                    Tetris.hold(initial_game_state)
                 )
             );
             const final_piece = final_game.current_tetromino;
@@ -28,19 +28,19 @@ describe("Hold", function () {
     );
 
     it(
-        `Hold can't be performed twice in a row:
-        Given a Tetris Game where a Hold is performed;
-        When one further Hold is performed;
-        Then the game state before and after the second hold, is the same.`,
+        `The Hold function can only be carried out once, not twice successively:
+        In the event when a Tetris Game is running and the hold function is carried out;
+        When one more Hold function is performed;
+        The game state before and after the second hold is unchanged.`,
         function () {
-            const initial_game = Tetris.hold(Tetris.new_game());
-            const held_game = Tetris.hold(initial_game);
+            const initial_game_state = Tetris.hold(Tetris.new_game());
+            const held_game_state = Tetris.hold(initial_game_state);
 
-            if (!R.equals(initial_game, held_game)) {
+            if (!R.equals(initial_game_state, held_game_state)) {
                 throw new Error(
                     `The inital and final game state do not match
-                    Initial: display game: ${JSON.stringify(initial_game)}
-                    Final: display game   ${JSON.stringify(held_game)}`
+                    Initial: ${JSON.stringify(initial_game_state)}
+                    Final: ${JSON.stringify(held_game_state)}`
                 );
             }
 
@@ -56,20 +56,19 @@ describe("Hold", function () {
         Then the current tetrimino is equal to the previous next piece
         `,
         function () {
-            const initial_game = Tetris.new_game();
-            const initial_piece = initial_game.current_tetromino;
-            const held_game = Tetris.hold(initial_game);
-
-            const held_current_tet = held_game.current_tetromino
-            const initial_next_tet = initial_game.next_tetromino
+            const initial_game_state = Tetris.new_game();
+            const initial_piece = initial_game_state.current_tetromino;
+            const held_game_state = Tetris.hold(initial_game_state);
+            const held__current_tetromino = held_game_state.current_tetromino
+            const initial__next_tetromino = initial_game_state.next_tetromino
 
             // You'll need to implement Tetris.hold before this works.
             //const final_piece = final_game.current_tetromino;
-            if (!R.equals(held_game.current_tetromino, initial_game.next_tetromino)) {
+            if (!R.equals(held_game_state.current_tetromino, initial_game_state.next_tetromino)) {
                 throw new Error(
                     `The inital and final tetrominos do not match
-                    Initial: ${JSON.stringify(held_current_tet)}
-                    Final:   ${JSON.stringify(initial_next_tet)}`
+                    Initial: ${JSON.stringify(held__current_tetromino)}
+                    Final:   ${JSON.stringify(initial__next_tetromino)}`
                 );
             }
         }
